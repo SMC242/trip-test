1f73faefe5ac2233cc548bda8337213726c30ac3
eilidh <56912943+SMC242@users.noreply.github.com>
2024-03-31 06:07:22 +0100

    chore: set up react query + react-hot-toast

---
+++ src/app/_contexts/providers.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      {children}
+    </QueryClientProvider>
+  );
+}
