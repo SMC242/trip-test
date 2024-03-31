73592082b3733b371b0648c19127a885d5a97b26
eilidh <56912943+SMC242@users.noreply.github.com>
2024-03-31 03:52:55 +0100

    feat: rendering map with leaflet. Closes #1

---
+++ src/app/_components/map.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+type MapProps = {
+  map: {
+    attribution: string;
+    url: string;
+  };
+} & React.ComponentProps<typeof MapContainer>;
+
+// From https://react-leaflet.js.org/
+export const defaultMap = {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+};
+
+export default function Map({ map, ...props }: MapProps) {
+  console.log("rendering");
+  return (
+    <MapContainer {...props} className="h-full w-full">
+      <TileLayer url={map.url} attribution={map.attribution} />
+    </MapContainer>
+  );
+}
