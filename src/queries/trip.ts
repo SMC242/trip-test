1c4db8f41311828962fc9a1231b8b5b581ee99c1
eilidh <56912943+SMC242@users.noreply.github.com>
2024-03-31 02:26:54 +0100

    feat: trip request

---
+++ src/queries/trip.ts
@@ -0,0 +1,9 @@
+import { Trip, type Trip as TripT } from "@/models/trip";
+import env from "@/utils/env";
+import { query } from "@/utils/request";
+
+export async function getTrip(
+  tripId: string
+): Promise<ReturnType<typeof query<TripT>>> {
+  return await query(`${env.API_BASE_URL}/trips/${tripId}`, {}, Trip);
+}
