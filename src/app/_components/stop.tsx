b75047b6559d6971b07d790fd0004022d4cc087f
SMC242 <56912943+SMC242@users.noreply.github.com>
2024-04-18 18:01:19 +0100

    refactor: clean up getIconURL

--- src/app/_components/stop.tsx
+++ src/app/_components/stop.tsx
@@ -16,20 +16,17 @@ type StopProps = {
   size?: number;
 };
 
-function getIconURL(variant: StopProps["variant"]) {
+function getIconURL(variant: StopVariant) {
   const BASE_URL = "/images/";
-  switch (variant) {
-    case "origin":
-      return `${BASE_URL}origin-stop.svg`;
-    case "destination":
-      return `${BASE_URL}destination-stop.svg`;
-    case "next-stop":
-      return `${BASE_URL}next-stop.svg`;
-    case "previous-stop":
-      return `${BASE_URL}previous-stop.svg`;
-    default:
-      return `${BASE_URL}default-stop.svg`;
-  }
+  const variantFileNames: Record<StopVariant, string> = {
+    origin: "origin-stop.svg",
+    destination: "destination-stop.svg",
+    "next-stop": "next-stop.svg",
+    "previous-stop": "previous-stop.svg",
+    default: "default-stop.svg",
+  };
+
+  return `${BASE_URL}/${variantFileNames[variant]}`;
 }
 
 export default function Stop({
